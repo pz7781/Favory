d1ad5bdbaf7dcab715661f8f6ee1ec121b2bf6f3
pz7781 <pnoel7781@gmail.com>
2025-10-18 15:12:10 +0900

    first commit

---
+++ src/main/kotlin/project/favory/FavoryApplication.kt
@@ -0,0 +1,11 @@
+package project.favory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FavoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<FavoryApplication>(*args)
+}
---
+++ src/test/kotlin/project/favory/FavoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package project.favory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FavoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
