41ec3885fd7b1eecf9cb418b56ddb40050ecff2e
pz7781 <pnoel7781@gmail.com>
2026-01-05 21:20:25 +0900

    feat: hibernate에서 jpa annotation으로 변경, NOT NULL 설정

--- src/main/kotlin/project/favory/FavoryApplication.kt
+++ src/main/kotlin/project/favory/FavoryApplication.kt
@@ -2,10 +2,12 @@ package project.favory
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class FavoryApplication
 
 fun main(args: Array<String>) {
-	runApplication<FavoryApplication>(*args)
+    runApplication<FavoryApplication>(*args)
 }
